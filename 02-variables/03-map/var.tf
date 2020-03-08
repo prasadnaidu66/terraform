e55f001ef0d4bc94893a338ad13e6df792950bcc
prasadnaidu66 <prasadnaidu66@gmail.com>
2020-03-08 22:06:53 +0530

    MAP variables

---
+++ 02-variables/03-map/var.tf
@@ -0,0 +1,15 @@
+variable "TIMMINGS"{
+  default = {
+    MORNG = "8:30AM"
+    EVNG  = "6:30PM"
+
+  }
+}
+
+output "MRNG_TIMMINGS" {
+  value = var.TIMMINGS[MORNG]
+}
+
+output "EVN TIMMINGS" {
+  value = var.TIMMINGS[EVNG]
+}
