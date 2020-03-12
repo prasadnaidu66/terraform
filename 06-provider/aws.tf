b7e1af419c4cf35c5e3fcc33f9f9c62aa5a840e4
prasadnaidu66 <prasadnaidu66@gmail.com>
2020-03-12 22:22:10 +0530

    kk

--- 06-provider/aws.tf
+++ 06-provider/aws.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_instance" "web" {
   ami           = "ami-01b8d3897dce95258"
   instance_type = "t2.micro"
-  key_name      = "devops"
+  key_name      = "TEST3"
 
   tags = {
     Name = "HelloWorld"
