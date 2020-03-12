99108338629bc8cd5f6e774473fdc4931244e2b0
prasadnaidu66 <prasadnaidu66@gmail.com>
2020-03-12 22:47:08 +0530

    kk

---
+++ 07-data-sources/aws1.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region        = "us-east-1"
+}
+
+data "aws_ami" "default" {
+  owners        = ["973714476881"]
+  most_recent   = true
+  filter {
+    name        = "name"
+    values      = ["Centos-7-DevOps-Practice"]
+  }
+}
+
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.default.id
+  instance_type = "t2.micro"
+  key_name      = "TEST3"
+
+  tags          = {
+    Name        = "HelloWorld"
+  }
+}
+
+output "PUBLIC_IP_OF_INSTANCE" {
+  value         = aws_instance.web.public_ip
+}
