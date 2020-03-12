0a58cff4d5aba7ded6e6d26eb08a2a27d1bd5ac8
prasadnaidu66 <prasadnaidu66@gmail.com>
2020-03-12 22:19:47 +0530

    kk

---
+++ 06-provider/aws.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-01b8d3897dce95258"
+  instance_type = "t2.micro"
+  key_name      = "devops"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+output "PUBLIC_IP_OF_INSTANCE" {
+  value = aws_instance.web.public_ip
+}
