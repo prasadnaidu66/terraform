673450f969477b8d0da033c984d9ead34a600ecf
prasadnaidu66 <prasadnaidu66@gmail.com>
2020-03-12 21:54:00 +0530

    kk

--- 05-resources/resource.tf
+++ 05-resources/resource.tf
@@ -2,3 +2,13 @@ resource "local_file" "foo" {
   content = "foo"
   filename = "/tmp/abcd"
 }
+
+
+output "FILE_NAME" {
+  value = local_file.foo.filename
+}
+
+
+output "FILE_NAME_PERMISION" {
+  value = local_file.foo.file_permission
+}
