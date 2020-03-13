30b27a9b398451c224cd534384e2f94c5dc4424f
prasadnaidu66 <prasadnaidu66@gmail.com>
2020-03-13 22:48:05 +0530

    kk

---
+++ 10-remote-states/aws.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region         = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket       = "d45-terraform-states-1"
+    key          = "10-remote-states/terraform.tfstate"
+    region       = "us-east-1"
+    lock_table   = ""
+  }
+}
